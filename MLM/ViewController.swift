c24e83768e8abec3aa7eab602468884b0944383d
prit280 <neilpatel@NeilPatelsMBP3.attlocal.net>
2015-08-24 21:55:15 -0700

    Initial Commit

---
+++ MLM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MLM
+//
+//  Created by Neil Patel on 8/24/15.
+//  Copyright (c) 2015 MLM Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
